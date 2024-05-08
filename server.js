e1477d7730413f40e264838f82cf25b157164a2d
Jesbin <jesbin@techfriar.com>
2024-05-08 17:42:42 +0530

    node code updated

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('Server started');
+import express from "express";
+
+const app = express();
+
+const port = 3000;
+
+const hostname = "localhost";
+
+app.listen(port, hostname, () => {
+  console.log(`Server is running at http://${hostname}:${port}/`);
+});
